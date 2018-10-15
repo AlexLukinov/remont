dafeb5d518fb2f942062c71247dfaa8c1bb26545
Алексей Лукинов <lukinov@infocom.local>
2018-10-15 09:11:20 +0300

    afdgadfg

--- wp-content/themes/remont/js/index.js
+++ wp-content/themes/remont/js/index.js
@@ -1,18 +1,22 @@
 $(document).ready(function() { // вся мaгия пoсле зaгрузки стрaницы
-    var isMobile = window.matchMedia("only screen and (max-width: 768px)").matches;
-
-    // $( window ).resize(function() {
-    //     $( "#zayavka_text" ).css('height', '30%');
-    // });
-
-    initWhiteDivSize();
-
-    $('#white-div-watch').width($('#white-div-watch').width() + 114);
-
     $('.section_services').addClass("hidden").viewportChecker({
         classToAdd: 'visible animated fadeIn',
         offset: 100
     });
+
+    var isMobile = window.matchMedia("only screen and (max-width: 768px)").matches;
+
+    // slider line length in pixels
+    var sliderLineLength = 144;
+
+
+
+    //////////////////////////////////////
+    // HEADER SLIDER CONTENT HANDLE START
+    //////////////////////////////////////
+    initWhiteDivSize();
+
+    $('#white-div-watch').width($('#white-div-watch').width() + 114);
 
     // swiper
     var swiper = initSwiper();
@@ -24,137 +28,6 @@ $(document).ready(function() { // вся мaгия пoсле зaгрузки с�
         activeSwiperSlide.width('77%');
     } else {
         activeSwiperSlide.width('47%');
-    }
-
-    var currHeaderSlide = 1;
-    var currArticleSlide = 1;
-    var currReviewSlide = 1;
-
-    // all cards are displayed none, display flex first slides
-    setTimeout(function () {
-        if (isMobile) {
-            $('#article-card1').css('display', 'flex');
-            $('#review-card1').css('display', 'flex');
-        } else {
-            $('#article-card1').css('display', 'flex');
-            $('#article-card2').css('display', 'flex');
-            $('#article-card3').css('display', 'flex');
-            $('#review-card1').css('display', 'flex');
-            $('#review-card2').css('display', 'flex');
-            $('#review-card3').css('display', 'flex');
-            $('#review-card4').css('display', 'flex');
-        }
-    }, 500);
-    
-    const articlesCards = $('#blog .blog_card');
-    const articlesCount = articlesCards.length;
-    
-    const reviewsCards = $('.reviews_block_item');
-    const reviewsCount = reviewsCards.length;
-
-    var articlesSlidesCount = Math.floor(articlesCount / 3) + 1;
-
-    var reviewsSlidesCount = Math.floor(reviewsCount / 4) + 1;
-
-    function calcOneDevision(el, slidesCount) {
-        console.log(el.width());
-        console.log(slidesCount);
-        return el.width() / slidesCount;
-    }
-
-    function getOneDevisionByEl(el) {
-        switch (el) {
-            case 'article':
-                console.log('article: ' + calcOneDevision($("#article-svg"), articlesSlidesCount));
-                return calcOneDevision($("#article-svg"), articlesSlidesCount);
-                break;
-            case 'review':
-                console.log('review: ' + calcOneDevision($("#review-svg"), reviewsSlidesCount));
-                return calcOneDevision($("#review-svg"), reviewsSlidesCount);
-                break;
-            case 'header':
-                return calcOneDevision($("#header-svg"), 4);
-                break;
-        }
-    }
-
-    initSlider('article', articlesCards, articlesSlidesCount);
-    listenSwitchSlideClick('article', articlesSlidesCount, currArticleSlide);
-    
-    initSlider('review', reviewsCards, reviewsSlidesCount);
-    listenSwitchSlideClick('review', reviewsSlidesCount, currReviewSlide);
-
-    function initAutoSwitchHeaderSlides() {
-        setInterval(function () {
-            $('#swiper-button-next')[0].click();
-        }, 4000);
-    }
-    
-    function listenSwitchSlideClick(el, slidesCount, currSlide) {
-        let cards = (el == 'article') ? articlesCards : reviewsCards;
-        $('#next-' + el).click(function (e) {
-            e.preventDefault();
-            if (currSlide < slidesCount) {
-                currSlide += 1;
-                slideChange(el, currSlide);
-                handleCardsVisibility(el, cards, currSlide);
-            }
-        });
-        
-        $('#prev-' + el).click(function (e) {
-            e.preventDefault();
-            if (currSlide > 1) {
-                currSlide -= 1;
-                slideChange(el, currSlide);
-                handleCardsVisibility(el, cards, currSlide);
-            }
-        });
-    }
-
-    // handle switch of slides (animate line, text of current slide number)
-    function slideChange(el, currSlide) {
-        let oneDevision = getOneDevisionByEl(el);
-        handleSliderLine($('#' + el + 's-line'), oneDevision*currSlide);
-        let currSlideText = currSlide > 9 ? currSlide : '0' + currSlide;
-        $('#' + el + 's-curr-slide').text(currSlideText);
-    }
-
-    // toggle visibility of cards
-    function handleCardsVisibility(cardEl, cards, currSlide) {
-        cards.css('display', 'none');
-        if (isMobile) {
-            $('#' + cardEl + '-card' + currSlide).css('display', 'flex');
-        } else {
-            $('#' + cardEl + '-card' + currSlide*3).css('display', 'flex');
-            $('#' + cardEl + '-card' + (currSlide*3 - 1)).css('display', 'flex');
-            $('#' + cardEl + '-card' + (currSlide*3 - 2)).css('display', 'flex');
-            if (cardEl == 'review') {
-                $('#' + cardEl + '-card' + (currSlide*3 - 3)).css('display', 'flex');
-            }
-        }
-    }
-
-    // init slider (article and review)
-    function initSlider(el, cards, slidesCount) {
-        cards.css('display', 'none');
-
-        if (isMobile) {
-            slidesCount = (el == 'article') ? articlesCount : reviewsCount;
-        }
-
-        let countText = slidesCount > 9 ? slidesCount : '0' + slidesCount;
-        $('#' + el + 's-count').text(countText);
-
-        $('#' + el + 's-curr-slide').text('01');
-        handleSliderLine($('#' + el + 's-line'), getOneDevisionByEl(el));
-    }
-
-    // get line element and set it length
-    function handleSliderLine(line, length) {
-        if (length > 140) {
-            length = 144;
-        }
-        line.attr('d', 'M0 0, ' + length + ' 10');
     }
 
     // white div is open-close h2 in header
@@ -244,5 +117,151 @@ $(document).ready(function() { // вся мaгия пoсле зaгрузки с�
 
         return new Swiper(".swiper-container", swiperOptions);
     }
+    //////////////////////////////////////
+    // HEADER SLIDER CONTENT HANDLE FINISH
+    //////////////////////////////////////
+
+
+
+    //////////////////////////////////////
+    // SLIDERS HANDLE START
+    //////////////////////////////////////
+    makeDisplayFlexOnInit();
+
+    var currHeaderSlide = 1;
+    var currArticleSlide = 1;
+    var currReviewSlide = 1;
+
+
+    const articlesCards = $('#blog .blog_card');
+    const articlesCount = articlesCards.length;
+
+    const reviewsCards = $('.reviews_block_item');
+    const reviewsCount = reviewsCards.length;
+
+    var articlesSlidesCount = Math.floor(articlesCount / 3) + 1;
+    var reviewsSlidesCount = Math.floor(reviewsCount / 4) + 1;
+
+    initSlider('article', articlesCards, articlesSlidesCount);
+    listenSwitchSlideClick('article', articlesSlidesCount, currArticleSlide);
+
+    initSlider('review', reviewsCards, reviewsSlidesCount);
+    listenSwitchSlideClick('review', reviewsSlidesCount, currReviewSlide);
+
+    // switch slides automatically every 4 seconds
+    function initAutoSwitchHeaderSlides() {
+        setInterval(function () {
+            $('#swiper-button-next')[0].click();
+        }, 4000);
+    }
+
+    // listeners of switch slides click events
+    function listenSwitchSlideClick(el, slidesCount, currSlide) {
+        let cards = (el == 'article') ? articlesCards : reviewsCards;
+        $('#next-' + el).click(function (e) {
+            e.preventDefault();
+            if (currSlide < slidesCount) {
+                currSlide += 1;
+                slideChange(el, currSlide);
+                handleCardsVisibility(el, cards, currSlide);
+            }
+        });
+
+        $('#prev-' + el).click(function (e) {
+            e.preventDefault();
+            if (currSlide > 1) {
+                currSlide -= 1;
+                slideChange(el, currSlide);
+                handleCardsVisibility(el, cards, currSlide);
+            }
+        });
+    }
+
+    // handle switch of slides (animate line, text of current slide number)
+    function slideChange(el, currSlide) {
+        let oneDevision = getOneDevisionByEl(el);
+        handleSliderLine($('#' + el + 's-line'), oneDevision*currSlide);
+        let currSlideText = currSlide > 9 ? currSlide : '0' + currSlide;
+        $('#' + el + 's-curr-slide').text(currSlideText);
+    }
+
+    // toggle visibility of cards on switch slides
+    function handleCardsVisibility(cardEl, cards, currSlide) {
+        cards.css('display', 'none');
+        if (isMobile) {
+            $('#' + cardEl + '-card' + currSlide).css('display', 'flex');
+        } else {
+            for (let i = 0; i <= cards.length; i++) {
+                $('#' + cardEl + '-card' + (currSlide*3 - i)).css('display', 'flex');
+            }
+        }
+    }
+
+    // init slider (article and review)
+    function initSlider(el, cards, slidesCount) {
+        cards.css('display', 'none');
+
+        if (isMobile) {
+            slidesCount = (el == 'article') ? articlesCount : reviewsCount;
+        }
+
+        let countText = slidesCount > 9 ? slidesCount : '0' + slidesCount;
+        $('#' + el + 's-count').text(countText);
+
+        $('#' + el + 's-curr-slide').text('01');
+        handleSliderLine($('#' + el + 's-line'), getOneDevisionByEl(el));
+    }
+
+    // get line element and set it length
+    function handleSliderLine(line, length) {
+        if (length > sliderLineLenght * 0.95) {
+            length = sliderLineLength;
+        }
+        line.attr('d', 'M0 0, ' + length + ' 10');
+    }
+
+    function makeDisplayFlexOnInit() {
+        // all cards are displayed none, display flex first slides
+        setTimeout(function () {
+            if (isMobile) {
+                makeDisplayFlexCards('article', 1);
+                makeDisplayFlexCards('review', 1);
+            } else {
+                makeDisplayFlexCards('article', 3);
+                makeDisplayFlexCards('review', 4);
+            }
+        }, 500);
+    }
+
+    function makeDisplayFlexCards(el, cardsCount) {
+        for (let i = 1; i <= cardsCount; i++) {
+            $('#' + el + '-card' + i).css('display', 'flex');
+        }
+    }
+
+    function calcOneDevision(el, slidesCount) {
+        console.log(el.width());
+        console.log(slidesCount);
+        return el.width() / slidesCount;
+    }
+
+    function getOneDevisionByEl(el) {
+        switch (el) {
+            case 'article':
+                console.log('article: ' + calcOneDevision($("#article-svg"), articlesSlidesCount));
+                return calcOneDevision($("#article-svg"), articlesSlidesCount);
+                break;
+            case 'review':
+                console.log('review: ' + calcOneDevision($("#review-svg"), reviewsSlidesCount));
+                return calcOneDevision($("#review-svg"), reviewsSlidesCount);
+                break;
+            case 'header':
+                return calcOneDevision($("#header-svg"), 4);
+                break;
+        }
+    }
+    //////////////////////////////////////
+    // SLIDERS HANDLE FINISH
+    //////////////////////////////////////
 
 });
